4e5c186c6e78086dbfc7e1564998aed37fd22d40
Deborah Schirmer <dhschirmer@gmail.com>
2024-11-07 14:21:17 +0100

    Updated code 10

--- src/common/HelloWorld.java
+++ src/common/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Klassen!");
         System.out.println("Mer text");
-        System.out.println("Hello World !");
+        System.out.println("Hello World  !");
     }
 }
